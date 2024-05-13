97a88628f9ef5ba019814c6ea992a0051ef0f9ab
RXGabriel <gabriel2005rf@gmail.com>
2024-05-13 16:15:01 -0300

    feat: add login route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable no-undef */
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import "./App.scss";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
